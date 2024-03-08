094ba28478af5f59b2c6173b307b44ac3d03e8c4
kyro <thomasmccalister@gmail.com>
2024-03-08 15:13:17 -0600

    add react icons and styling

--- tomsitsolutions/src/components/Footnav/footnav.js
+++ tomsitsolutions/src/components/Footnav/footnav.js
@@ -1,15 +1,19 @@
 // Imports
 import React from 'react';
 import './footnav.css'; // Import the CSS file
+//react icons imports
+import { FaPhone } from "react-icons/fa";
+import { MdElectricalServices } from "react-icons/md";
+import { FcAbout } from "react-icons/fc";
 
 //main function
 
 const Footnav = ({ onButtonClick }) => {
     return (
       <div className="navbar">
-        <button onClick={() => onButtonClick(1)}>About Us</button>
-        <button onClick={() => onButtonClick(2)}>Services</button>
-        <button onClick={() => onButtonClick(3)}>Contact Us</button>
+        <button onClick={() => onButtonClick(1)}><FcAbout /></button>
+        <button onClick={() => onButtonClick(2)}><MdElectricalServices /></button>
+        <button onClick={() => onButtonClick(3)}><FaPhone /></button>
       </div>
     );
   };
