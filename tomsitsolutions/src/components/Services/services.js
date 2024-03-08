0e312383e45ce3fb4b157098960609a024777811
kyro <thomasmccalister@gmail.com>
2024-03-07 20:24:49 -0600

    add services component

---
+++ tomsitsolutions/src/components/Services/services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './services.css'; // Import the CSS file for styling
+
+const Services = () => {
+  return (
+    <div className="services-container">
+      <h2 className="services-title">Services</h2>
+      <ul className="services-list">
+        <li>Virus and Malware Removal</li>
+        <li>Data Backup and Recovery</li>
+        <li>Hardware Upgrades</li>
+        <li>Remote Support</li>
+        <li>IT Consulting</li>
+        <li>Mobile Device Support</li>
+        <li>Printer and Peripheral Setup</li>
+        <li>Website Design and Programming</li>
+        <li>Custom Software Solutions</li>
+        <li>IT Training</li>
+        <li>and many more...</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
